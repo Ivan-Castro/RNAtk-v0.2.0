2c616d3f7a12cac2f958c880a376f9ec9efae91f
Sergio Iván Castro <sivanc7@gmail.com>
2015-09-15 09:52:22 -0500

    Create __init__.py

---
+++ rnatk/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#TODO: add doctest or unittest to each function
+
+"""
+rnatk is a module focused to manipulate sequences, fasta files \
+and RNA secondary - tertiary structure
+"""
+
+import sys
+
+__all__ = ['dendro', 'fasta', 'seq', 'stats', 'structure2D', 'structure3D', 
+           'utils']
+__author__ = "Ivan Castro"
+__copyright__ = "Copyright 2015, rnatk Project"
+__license__ = "GPL"
+__version__ = "0.2.0"
+__email__ = "sivanc7@gmail.com"
+
+#Check python version
+if sys.version_info < (2, 7):
+    py_version = ".".join([str(n) for n in sys.version_info])
+    raise RuntimeError("Python-2.7 or greater is required, Python-%s used." % py_version)
